92adafee727b801c8747ee6e8347655ee4ff4638
Mariano <marianomgabriel@hotmail.com>
2019-11-19 19:58:42 -0300

    Preguntas Frecuentes y Perfil de Usuario

---
+++ Perfil del Usuario.php
@@ -0,0 +1,38 @@
+.<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="Css\Style.css">
+    <title></title>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  </head>
+  <body id="index">
+    <article id="index">
+      HOME /
+    </article>
+     <p><strong>Perfil del Usuario</strong>  </p>
+    <div class="conteiner">
+
+</div>
+<div class="usuario">
+  <img class="usuario" src="usuario.png" alt="">
+</div>
+<div class="usuario">
+  <article class="">
+    <strong>Eduardo Sanches</strong>
+  </article>
+  <br>
+  <article class="">
+    EduSanches@yahoo.com.ar
+  </article>
+  
+</div>
+
+  </body>
+</html>
